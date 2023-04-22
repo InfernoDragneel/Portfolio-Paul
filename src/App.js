145047f2ba0ef0d4aa2ef1dadcb33d5be61fb71d
Paul <moaye@Paul>
2023-04-22 17:05:00 +0200

    mon portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar';
+import Cards from './components/Cards';
+import Projets from './components/Projets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar />
+    
+    <Cards />
+<h1>PROJETS</h1> 
+    <Projets />
+    </>
+
+    
+      );
+      <>
+      
+      </>
+    
+  
 }
 
 export default App;
